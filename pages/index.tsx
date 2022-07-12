18f7e9bca512ca506f7cb0422c9d3392b0482e20
Jakub Pisula <jakub.pisula@wp.pl>
2022-07-12 18:15:35 +0200

    Added homepage forst changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import Header from 'components/header/Header';
 import type { NextPage } from 'next';
+import wave from 'assets/svg/wave.svg';
 import styles from './index.module.scss';
 
 const Home: NextPage = () => {
+  console.log(wave);
   return (
     <>
       <Header />
-      Hello world
+      <section className={styles.landing}></section>
+
+      <section className={styles.promo}>
+        <img src={wave.src} />
+      </section>
     </>
   );
 };
