6b1703f9287a4c03a3ddb29bc53532721a2d20b5
Jakub Pisula <jakub.pisula@wp.pl>
2022-07-16 02:43:39 +0200

    Works on landing - button

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import Logo from 'public/GS_LOGO.svg';
+import { useEffect, useState } from 'react';
 import styles from './Header.module.scss';
 import Burger from './sub-components/Burger/Burger';
 
@@ -9,9 +10,28 @@ export interface INav {
 }
 
 const Header = () => {
+  const [navbarScrollStyles, setNavbarScrollStyles] = useState(false);
+
+  const changeNavbarStyles = () => {
+    if (window.scrollY >= 80) {
+      setNavbarScrollStyles(true);
+    } else {
+      setNavbarScrollStyles(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNavbarStyles);
+  }, []);
+
   return (
     <>
-      <nav className={styles.nav}>
+      <nav
+        className={classNames(
+          styles.nav,
+          navbarScrollStyles && styles.navbarScrolled
+        )}
+      >
         <div className={classNames('container', styles.navHeight)}>
           <div className={styles.navContent}>
             <div className={styles.logo}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,30 @@ import Header from 'components/header/Header';
 import type { NextPage } from 'next';
 import wave from 'assets/svg/wave.svg';
 import styles from './index.module.scss';
+import classNames from 'classnames';
 
 const Home: NextPage = () => {
   console.log(wave);
   return (
     <>
       <Header />
-      <section className={styles.landing}></section>
+      <section className={styles.landing}>
+        <div className={classNames('container', styles.landingContainer)}>
+          <div className={styles.welcomeText}>
+            <h1>
+              Szukasz szansy na <mark className={styles.animSpan1}>rozwój</mark>{' '}
+              swojej <mark className={styles.animSpan2}>marki</mark>?
+            </h1>
+            <h2>
+              Pomożemy Ci zbudować stronę Twoich marzeń, dzięki której{' '}
+              <mark className={styles.animSpan3}>każdy o Tobie usłyszy</mark>!
+            </h2>
+            <button role='button'>
+              <span className='p'>Poznaj nasze wartości</span>
+            </button>
+          </div>
+        </div>
+      </section>
 
       <section className={styles.promo}>
         <img src={wave.src} />
